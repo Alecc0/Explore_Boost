5a2970668e1475408b38161cdbcc244043a67136
Alex Tabbal <atabbal@gmail.com>
2015-02-19 13:25:41 -0700

    Adding a file from my windows box.
    First attempt to add a file to the project.

---
+++ Multi-Index-Containers/Multi-Index-Containers.cpp
@@ -0,0 +1,12 @@
+// Multi-Index-Containers.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "boost\multi_index\composite_key.hpp"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
